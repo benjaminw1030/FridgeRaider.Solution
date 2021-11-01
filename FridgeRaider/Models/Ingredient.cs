dbd7bb3ac261427f80265859d1b3b4d0df9ba397
Ben Wilson <benjaminw1030@gmail.com>
2021-11-01 14:08:19 -0700

    Set up ingredient.cs
    Co-authored-by: Scott Hutley <scotthutley1@comcast.net>
    Co-authored-by: Art Weinstein <artur.weinstein@gmail.com>
    Co-authored-by: Erica Marroquin <ericamarroquin03@gmail.com>

--- FridgeRaider/Models/Ingredient.cs
+++ FridgeRaider/Models/Ingredient.cs
@@ -1,7 +1,21 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace FridgeRaider.Models
 {
   public class Ingredient
   {
+    public string IdIngredient { get; set; }
+    public string StrIngredient { get; set; }
 
+    public static List<Ingredient> GetIngredients(string apiKey)
+    {
+      var apiCallTask = ApiHelper.GetIngredients(apiKey);
+      var result = apiCallTask.Result;
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      List<Ingredient> ingredientList = JsonConvert.DeserializeObject<List<Ingredient>>(jsonResponse["meals"].ToString());
+      return ingredientList;
+    }
   }
 }
