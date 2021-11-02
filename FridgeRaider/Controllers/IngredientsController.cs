aea5e9e79c7c549aace91c44056f31174172f714
Scott Hutley <scotthutley1@comcast.net>
2021-11-02 10:17:03 -0700

    WIP

--- FridgeRaider/Controllers/IngredientsController.cs
+++ FridgeRaider/Controllers/IngredientsController.cs
@@ -5,22 +5,22 @@ using System.Collections.Generic;
 using System.Linq;
 using FridgeRaider.Models;
 
-namespace FridgeRaider.Controllers
-{
-  public class IngredientsController : Controller
-  {
-    private readonly FridgeRaiderContext _db;
+// namespace FridgeRaider.Controllers
+// {
+//   public class IngredientsController : Controller
+//   {
+//     private readonly FridgeRaiderContext _db;
 
-    public IngredientsController(FridgeRaiderContext db)
-    {
-      _db = db;
-    }
+//     public IngredientsController(FridgeRaiderContext db)
+//     {
+//       _db = db;
+//     }
 
-    public ActionResult Index()
-    {
-      return View(_db.Ingredients.ToList());
-    }
+//     public ActionResult Index()
+//     {
+//       return View(_db.Ingredients.ToList());
+//     }
 
 
-  }
-}
+//   }
+// }
