359cf494b30ba7db9b333cbf17f41af34469a2a1
Scott Hutley <scotthutley1@comcast.net>
2021-11-02 09:43:50 -0700

    WIP
    Co-authored-by: Erica Marroquin <ericamarroquin@users.noreply.github.com>

---
+++ FridgeRaider/Controllers/IngredientsController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore;
+using System.Collections.Generic;
+using System.Linq;
+using FridgeRaider.Models;
+
+namespace FridgeRaider.Controllers
+{
+  public class IngredientsController : Controller
+  {
+    private readonly FridgeRaiderContext _db;
+
+    public IngredientsController(FridgeRaiderContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Ingredients.ToList());
+    }
+
+
+  }
+}
