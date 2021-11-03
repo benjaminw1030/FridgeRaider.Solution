f913d64906acdf52ce50509623c493de2801120d
Scott Hutley <scotthutley1@comcast.net>
2021-11-03 13:22:32 -0700

    Add Details to IngredientsController
    Co-authored-by: Erica Marroquin <ericamarroquin03@gmail.com>

--- FridgeRaider/Controllers/IngredientsController.cs
+++ FridgeRaider/Controllers/IngredientsController.cs
@@ -5,13 +5,24 @@ using Microsoft.AspNetCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 using FridgeRaider.Models;
 
 namespace FridgeRaider.Controllers
 {
   public class IngredientsController : Controller
   {
-
+    private readonly FridgeRaiderContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    
+    public IngredientsController(UserManager<ApplicationUser> userManager, FridgeRaiderContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+    
     public ActionResult Index(string letter)
     {
       var allIngredients = Ingredient.GetIngredients(EnvironmentVariables.ApiKey).ToList();
@@ -22,6 +33,26 @@ namespace FridgeRaider.Controllers
         .ToList()
         .Where(x => x.StrIngredient[0].ToString() == letter);
       return View(ingredients);
+    }
+
+    public IActionResult Details(string id)
+    {
+      var ingredientDetails = Ingredient.GetIngredients(EnvironmentVariables.ApiKey).ToList()
+      .Where(x => x.IdIngredient == id).ToList();
+      return View(ingredientDetails);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> AddIngredient(ApplicationUser user, string idIngredient)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      if (idIngredient != null)
+      {
+        _db.UserIngredients.Add(new UserIngredient() { UserId = currentUser.Id, IdIngredient = idIngredient });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
